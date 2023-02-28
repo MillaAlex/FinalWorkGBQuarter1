acdaccd736dce3a10ff5ddab337abc2727f2459d
Lyudmila <liudmila.k1610@gmail.com>
2023-03-01 00:28:34 +0300

    added fillArray and printArray methods with condition : value length less/equal 3

--- StringArray/Program.cs
+++ StringArray/Program.cs
@@ -1,2 +1,24 @@
 ﻿string[] arrayString = {"hello", "2", "world", ":-)", "-2", "999", "hey", "hi"};
 string[] arrayNewString = new string[arrayString.Length];
+
+void FillShortArray(string[] arrayString, string[] arrayNewString)
+{
+    int count = 0;
+    for (int i = 0; i < arrayString.Length; i++)
+    {
+    if (arrayString[i].Length <= 3)
+        {
+        arrayNewString[count] = arrayString[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+FillShortArray(arrayString, arrayNewString);
+PrintArray(arrayNewString);
